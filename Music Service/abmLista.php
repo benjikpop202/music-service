47e4b6c4255f8c5838814147387b69d26e282821
benjikpop202 <benjaminsobarzo2018@gmail.com>
2023-08-28 01:31:09 -0300

    json lista

--- Music Service/abmLista.php
+++ Music Service/abmLista.php
@@ -38,11 +38,33 @@ class Lista{
            
            echo( "*".$cancion->getCancion()."\n");
            }
-           
-           
             
         
         }
+        public function toJson() {
+            return json_encode($this->toArray());
+        }
+        public static function fromJson($json) {
+            $listaData = json_decode($json, true);
+            return self::fromArray($listaData);
+        }
+        public function toArray() {
+            return [
+                'nombre' => $this->Nombre,
+                'canciones' => $this->Canciones,
+                
+            ];
+        }
+        public static function fromArray($data) {
+            $lista = new Lista($data['nombre']);
+            foreach ($data['canciones'] as $cancionData) {
+                $cancion = new Cancion($cancionData['nombre'], $cancionData['artista'], $cancionData['genero']);
+                $lista->guardarCancion($cancion);
+            }
+            return $lista;
+        }
+        
+                       
     }
  
 
