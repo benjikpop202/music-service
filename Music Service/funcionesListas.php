3dcaae37709dd86c05ff8864f494efc7cd8ce2a4
benjikpop202 <benjaminsobarzo2018@gmail.com>
2024-03-13 13:10:09 -0300

    add changes genere list

--- Music Service/funcionesListas.php
+++ Music Service/funcionesListas.php
@@ -21,32 +21,49 @@ function CreateVoidList($user){
 
 function CreateGenereList($user){
  global $conexion;
- write("crea tu lista generica");
- write("======================");
- $lista = readline("ingrese nombre para lista generica: ");
- $genero = readline("ingrese el genero para la lista: ");
- if($lista != null || $genero != null){
-    $stmt = $conexion->prepare('INSERT INTO listas (nombre, es_publica, usuario_id) VALUES (?, ?, ?) ');
-    $stmt->execute([$lista, 'false', $user->getID()]);
-    $listaID = $conexion->lastInsertId();
-    $newlista = new lista($listaID,$lista, $conexion);
-    $user->Guardar($newlista);
-    //traer las canciones de la base de datos
-    $stmt2 = $conexion->prepare('SELECT id, titulo, artista, genero FROM canciones WHERE genero LIKE :genero ORDER BY RANDOM() LIMIT 10 ');
-    $stmt2->bindValue(':genero', '%' . $genero . '%', PDO::PARAM_STR);
-    $stmt2->execute();
-    $rows = $stmt2->fetchALL(PDO::FETCH_ASSOC);
-    foreach($rows as $row){
-        $id = $row['id'];
-        $titulo = $row['titulo'];
-        $artista = $row['artista'];
-        $genero = $row['genero'];
-
+ if($user->getStatus()=="premium"){
+    write("crea tu lista generica");
+    write("======================");
+    $lista = readline("ingrese nombre para lista generica: ");
+    $genero = readline("ingrese el genero para la lista: ");
+    if($lista != null || $genero != null){
+       $stmt = $conexion->prepare('INSERT INTO listas (nombre, es_publica, usuario_id) VALUES (?, ?, ?) ');
+       $stmt->execute([$lista, 'false', $user->getID()]);
+       $listaID = $conexion->lastInsertId();
+       $newlista = new lista($listaID,$lista, $conexion);
+       $user->Guardar($newlista);
+       //traer las canciones de la base de datos
+       $stmt2 = $conexion->prepare('SELECT c.id, c.titulo, c.artista, c.genero FROM canciones c JOIN listas l ON c.lista_id = l.id WHERE l.es_publica = true AND c.genero LIKE :genero ORDER BY RANDOM() LIMIT 5 ');
+       $stmt2->bindValue(':genero', '%' . $genero . '%', PDO::PARAM_STR);
+       $stmt2->execute();
+       $rows = $stmt2->fetchALL(PDO::FETCH_ASSOC);
+       foreach($rows as $row){
+           $titulo = $row['titulo'];
+           $artista = $row['artista'];
+           $genero = $row['genero'];
+           //crear copias de canciones
+           $stmt3 = $conexion->prepare('INSERT INTO canciones (titulo, artista, genero, lista_id) VALUES (?, ?, ?, ?) ');
+           $stmt3->execute([$titulo, $artista, $genero, $newlista->getID()]);
+       }
+       $newCancion = $conexion->prepare("SELECT c.id, c.titulo, c.artista, c.genero FROM canciones c WHERE c.lista_id = :id");
+       $newCancion->bindValue(':id', $newlista->getID());
+       $newCancion->execute();
+       $filas = $newCancion->fetchALL(PDO::FETCH_ASSOC);
+       foreach($filas as $fila){
+        $id = $fila['id'];
+        $titulo = $fila['titulo'];
+        $artista = $fila['artista'];
+        $genero = $fila['genero'];
         $cancion = new Cancion($id, $titulo, $artista, $genero);
         $newlista->guardarCancion($cancion);
     }
+    write("lista creada");
+    }
+ }else{
+    write("opcion premium");
  }
-}
+ 
+ }
 function crearListaDeListas($user) {
     global $conexion;
     $NewLista = readline("ingrese nombre de lista");
